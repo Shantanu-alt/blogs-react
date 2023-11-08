47140b36cdaa355560d0612c72fb1904ec8ad648
Shantanu Vedante <121545998+Shantanu-alt@users.noreply.github.com>
2023-11-08 12:08:16 +0000

    shantanu_updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,6 +5,7 @@ const About = () => {
     <div className="container padding">
       <div className="col-md-12">
         <div className="row mx-0">
+          <img src="logo.png"></img>
           <p>
             Swarajya Prathisthan is an Registered NGO Trust which carry out various donation drives of food , clothes and many more. Also Money are provided to need people for education purpose.
           </p>
